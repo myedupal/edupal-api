f3ac4750de687c36c543fbfb73a4816c8bae26ac
Gavin Moh <8167652+gavinmoh@users.noreply.github.com>
2024-03-05 13:26:37 +0800

    Fixed token_authenticatable strategy triggered during login request when bearer token is present

--- lib/token_authenticatable/strategy.rb
+++ lib/token_authenticatable/strategy.rb
@@ -2,17 +2,17 @@ require 'devise'
 
 module TokenAuthenticatable
   class Strategy < Devise::Strategies::Authenticatable
-
     def valid?
-      env['HTTP_AUTHORIZATION'].present?
+      env['HTTP_AUTHORIZATION'].present? && authenticate_request?(request)
     end
 
     def authenticate!
       return success!(warden_user)   if warden_user # check if user already set by another strategy
-      return fail!(:unauthenticated) if !(session = Session.find_by(token: bearer_token, scope: scope.to_s))
-      return fail!(:timeout)         if (session.revoked? or session.expired?)
-      return fail!(:inactive)        if !session.account.active_for_authentication?
-      return success!(session.account)
+      return fail!(:unauthenticated) unless (session = Session.find_by(token: bearer_token, scope: scope.to_s))
+      return fail!(:timeout)         if session.revoked? or session.expired?
+      return fail!(:inactive)        unless session.account.active_for_authentication?
+
+      success!(session.account)
     end
 
     def store?
@@ -20,18 +20,29 @@ module TokenAuthenticatable
     end
 
     private
+
       def bearer_token
         pattern = /^Bearer /
         header  = env['HTTP_AUTHORIZATION']
-        if header && header.match(pattern)
-          header.gsub(pattern, '') 
-        else
-          nil
-        end
+        return unless header && header.match(pattern)
+
+        header.gsub(pattern, '')
       end
 
       def warden_user
         env['warden'].user
+      end
+
+      def authenticate_request?(request)
+        self.class.module_parent.configuration.creator_requests.each do |matcher|
+          request_method, pattern = matcher
+          if pattern.is_a?(Regexp)
+            return false if request_method == request.method && request.path.match(pattern)
+          elsif request_method == request.method && request.path == pattern
+            return false
+          end
+        end
+        true
       end
   end
 end
