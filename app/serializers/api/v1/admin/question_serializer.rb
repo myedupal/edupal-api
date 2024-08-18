8b413cb81b7ef99df435257f2b913dac13199d3e
Gavin Moh <8167652+gavinmoh@users.noreply.github.com>
2024-08-18 20:57:23 +0800

    Added metadata to admin questions serializer

--- app/serializers/api/v1/admin/question_serializer.rb
+++ app/serializers/api/v1/admin/question_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::Admin::QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :number, :question_type, :topics_label, :text, :exam_id, :subject_id
+  attributes :id, :number, :question_type, :topics_label, :text, :exam_id, :subject_id, :metadata
   attributes :created_at, :updated_at
   has_one :exam
   has_one :subject
